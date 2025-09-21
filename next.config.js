d304662e35c1d4917df94f6267f523f017a3fa6e
shivanamdeo23-cpu <shivanamdeo23@gmail.com>
2025-09-21 19:48:14 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  reactStrictMode: true
 };
 module.exports = nextConfig;
