36aa2b604cc24bd73f95f37876b5b610a435377d
shivanamdeo23-cpu <shivanamdeo23@gmail.com>
2025-09-21 09:32:14 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "2rem",
+      }}
+    >
+      Welcome to ReGain
+    </main>
+  );
+}
